dff77fae3860ed6bb06d42896562436bd35badc1
Antoine FORET <antoine@foret.dev>
2019-11-16 15:56:11 +0100

    Add digit display

--- FooBarQix/specs/foo_bar_qix_spec.rb
+++ FooBarQix/specs/foo_bar_qix_spec.rb
@@ -9,15 +9,15 @@ RSpec.describe 'FooBarQix' do
     end
 
     it 'should return \'Foo\' on 3' do
-      expect(subject.compute('3')).to eql "Foo"
+      expect(subject.compute('3')).to include "Foo"
     end
 
     it 'should return \'Bar\' on 5' do
-      expect(subject.compute('5')).to eql "Bar"
+      expect(subject.compute('5')).to include "Bar"
     end
 
     it 'should return \'Qix\' on 7' do
-      expect(subject.compute('7')).to eql "Qix"
+      expect(subject.compute('7')).to include "Qix"
     end
 
     it 'should return \'Foo\' for a multiple of 3' do
@@ -37,7 +37,7 @@ RSpec.describe 'FooBarQix' do
     end
 
     it 'should display \'FooBar\' for a multiple of 3 and 5' do
-      expect(subject.compute('15')).to eql 'FooBar'
+      expect(subject.compute('60')).to eql 'FooBar'
     end
 
     it 'should display \'FooQix\' for a multiple of 3 and 7' do
@@ -45,11 +45,23 @@ RSpec.describe 'FooBarQix' do
     end
 
     it 'should display \'BarQix\' for a multiple of 5 and 7' do
-      expect(subject.compute('35')).to eql 'BarQix'
+      expect(subject.compute('140')).to eql 'BarQix'
     end
 
     it 'should display \'FooBarQix\' for a multiple of 3, 5 and 7' do
-      expect(subject.compute('105')).to eql 'FooBarQix'
+      expect(subject.compute('210')).to eql 'FooBarQix'
+    end
+
+    it 'should display \'Foo\' for each 3 digit' do
+      expect(subject.compute('13')).to eql 'Foo'
+    end
+
+    it 'should display \'Bar\' for each 5 digit' do
+      expect(subject.compute('59')).to eql 'Bar'
+    end
+
+    it 'should display \'Qix\' for each 7 digit' do
+      expect(subject.compute('71')).to eql 'Qix'
     end
   end
 end
--- FooBarQix/srcs/foo_bar_qix.rb
+++ FooBarQix/srcs/foo_bar_qix.rb
@@ -6,8 +6,21 @@ class FooBarQix
   ]
   
   def compute(str)
+    multiple_texts(str.to_f) + digit_texts(str.to_s)
+  end
+
+  private
+
+  def multiple_texts(value)
     MULTIPLE_RESPONSES.map do |responses|
-      responses[:text] if str.to_f % responses[:multiple] == 0
-    end.join('')
+      responses[:text] if value % responses[:multiple] == 0
+    end.join
+  end
+
+  def digit_texts(value)
+    value.split('').map do |char|
+      response = MULTIPLE_RESPONSES.detect { |response| response[:multiple] == char.to_i }
+      response[:text] unless response.nil?
+    end.join
   end
 end
